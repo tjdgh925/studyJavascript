73f7c5c3ea614d463c200b92362381deac2c9303
장성호 <tjdgh925@gmail.com>
2021-01-25 00:37:05 +0900

    17_SortWithoutArticle

---
+++ 17_SortWithoutArticle/arraySort.js
@@ -0,0 +1,20 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const list = document.querySelector('#bands');
+
+function strip(bandName){
+    return bandName.replace(/^(a |the |an)/i,'').trim();
+}
+
+const sortedBands = bands.sort(function (a, b){
+    if(strip(a) > strip(b)){
+        return 1;
+    } else{
+        return -1;
+    }
+});
+
+list.innerHTML = sortedBands.map(band =>
+    `
+        <li>${band}</li>
+    `
+).join('');
