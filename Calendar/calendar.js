2aa5c8d7a83e0bc06756c7bb49a2e532abeb98d5
장성호 <tjdgh925@gmail.com>
2021-02-06 00:38:34 +0900

    Calendar#3 print Calendar

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -3,17 +3,52 @@ const monthNames = [
 "JULY", "AUG", "SEP", "OCT", "NOV", "DEC"
 ];
 const monthSelect = document.querySelector('.monthSelect');
+const calendarTable = document.querySelector('table');
 var today = new Date();
 var first = new Date(today.getFullYear(), today.getMonth(),1);
 var change = first;
 var year = first.getFullYear();
 let month = first.getMonth();
+const endOfMonth = [31, 28, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31];
 
+//연도, 월 적용 함수
 function setMonth(){
     monthSelect.querySelector('span').innerText =
     `${monthNames[first.getMonth()]} ${first.getFullYear()}`
-    console.log(first);
 }
+//달력 출력
+function setDate(){
+    var cell;
+    var startOfMonth = first.getDay();
+    var todayDate = today.getDate();
+    var cnt = startOfMonth;
+    while (calendarTable.rows.length > 1)
+        calendarTable.deleteRow(calendarTable.rows.length-1);
+    
+    var row = calendarTable.insertRow();
+    
+    for(var i = 0; i < startOfMonth; i++){
+        cell = row.insertCell();
+    }
+    for(var i = 1; i < endOfMonth[first.getMonth()] + 1; i ++){
+        if(cnt === 7){
+            row = calendarTable.insertRow();
+            cnt = 0;
+        }
+        cell = row.insertCell();
+        cell.classList.add('day');
+        cell.innerHTML = i;
+        cnt++;
+        if(cnt%7 === 0)
+            cell.classList.add('saturday');
+        else if(cnt == 1)
+            cell.classList.add('sunday');
+        if(i === todayDate){
+            cell.classList.add('today');
+        }
+    }
+}
+//버튼 누를경우 연도, 월 바꾸는 함수
 function changeMonth(e){
     if(e.target.classList.contains('previous')){
         if(change.getMonth()-1 < 0){
@@ -37,7 +72,15 @@ function changeMonth(e){
     }
     else return;
     setMonth();
+    setDate();
 }
-
-monthSelect.addEventListener('click',changeMonth);
+function clickCalendar(e){
+    for(var i = 1; i <= endOfMonth[first.getMonth()]; i++)
+    if(e.target.classList.contains('day'))
+        e.target.classList.add('selected');
+    // if(e.target.)
+}
 setMonth();
+setDate();
+monthSelect.addEventListener('click',changeMonth);
+calendarTable.addEventListener('click', clickCalendar);
